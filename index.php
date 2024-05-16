67fa2b38351c594d56f779da993bf9380e72416e
Lorenzo Slulmeister <lorenzoslulmeister12@gmail.com>
2024-05-15 21:12:44 -0300

    Mi prime commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina primer commit</title>
+</head>
+<body>
+    <?php
+    print "mi primera pagina";
+    ?>
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, animi eum ratione accusamus soluta vel inventore alias numquam error possimus delectus, fugit qui odit cum ducimus earum enim, molestiae suscipit.
+
+</body>
+</html>
