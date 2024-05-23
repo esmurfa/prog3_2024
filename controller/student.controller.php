d965744000034ada2f5ec68da8cef0d1d855818c
Lorenzo Slulmeister <lorenzoslulmeister12@gmail.com>
2024-05-22 21:53:45 -0300

    22 de mayo

---
+++ controller/student.controller.php
@@ -0,0 +1,14 @@
+<?php 
+include("model/student.class.php");
+
+$operacion=$_REQUEST["opreacion"];
+
+if($operacion=="add"){
+
+}else if($operacion=="update"){
+
+}else if($operacion=="delete"){
+
+}
+
+?>
