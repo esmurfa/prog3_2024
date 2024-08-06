ad4cc081252fda24d32571101524be70d7020d43
Aron Mayer <121520247+AronMayer@users.noreply.github.com>
2024-08-06 18:03:23 -0300

    Add files via upload

---
+++ model/assistance.class.php
@@ -0,0 +1,29 @@
+<?php
+    class assistance{
+
+        private $id;
+        private $date;
+        private $present;
+
+        public function addAssistance(){
+
+        }
+
+        public function deleteAssistance(){
+            
+        }
+
+        public function updateAssistance(){
+            
+        }
+
+        public function getAssistance(){
+            
+        }
+
+        public function getAllAssistance(){
+            
+        }
+    }
+
+?>
