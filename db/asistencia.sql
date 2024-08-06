7a8852aa90c5b4c5fae132d13ebd407ecd8ebd28
Aron Mayer <121520247+AronMayer@users.noreply.github.com>
2024-08-06 19:03:09 -0300

    Update asistencia.sql
    Cambio de los campos a nombres en ingles

--- db/asistencia.sql
+++ db/asistencia.sql
@@ -1,28 +1,28 @@
-CREATE TABLE `asistencia` (
-  `idalumno` int(10) NOT NULL,
-  `turno` varchar(2) NOT NULL,
-  `fecha` varchar(10) NOT NULL,
-  `asistencia` varchar(2) NOT NULL,
-  `curso` int(4) NOT NULL,
+CREATE TABLE `assistance` (
+  `idStudent` int(10) NOT NULL,
+  `turn` varchar(2) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `assistance` varchar(2) NOT NULL,
+  `course` int(4) NOT NULL,
   `division` int(2) NOT NULL,
-  `anio_lectivo` int(4) NOT NULL,
-  `responsable` varchar(20) NOT NULL
+  `schoolYear` int(4) NOT NULL,
+  `responsible` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `asistencia_contraturno`
+-- Table structure for table `counter_shift_assistance`
 --
 
-CREATE TABLE `asistencia_contraturno` (
-  `idalumno` int(10) NOT NULL,
-  `turno` varchar(2) NOT NULL,
-  `fecha` varchar(10) NOT NULL,
-  `asistencia` varchar(2) NOT NULL,
+CREATE TABLE `counter_shift_assistance` (
+  `idStudent` int(10) NOT NULL,
+  `turn` varchar(2) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `assistance` varchar(2) NOT NULL,
   `materia` int(10) NOT NULL,
-  `curso` int(4) NOT NULL,
+  `course` int(4) NOT NULL,
   `division` int(2) NOT NULL,
-  `anio_lectivo` int(4) NOT NULL,
-  `responsable` varchar(20) NOT NULL
+  `schoolYear` int(4) NOT NULL,
+  `responsible` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
