229f0ca526a640653927873219efcff971b30567
Aron Mayer <121520247+AronMayer@users.noreply.github.com>
2024-08-06 19:06:26 -0300

    Update assistance.sql
    Cambios al Snake en espacios

--- db/assistance.sql
+++ db/assistance.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `assistance` (
-  `idStudent` int(10) NOT NULL,
+  `id_student` int(10) NOT NULL,
   `turn` varchar(2) NOT NULL,
   `date` varchar(10) NOT NULL,
   `assistance` varchar(2) NOT NULL,
   `course` int(4) NOT NULL,
   `division` int(2) NOT NULL,
-  `schoolYear` int(4) NOT NULL,
+  `school_year` int(4) NOT NULL,
   `responsible` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -16,13 +16,13 @@ CREATE TABLE `assistance` (
 --
 
 CREATE TABLE `counter_shift_assistance` (
-  `idStudent` int(10) NOT NULL,
+  `id_student` int(10) NOT NULL,
   `turn` varchar(2) NOT NULL,
   `date` varchar(10) NOT NULL,
   `assistance` varchar(2) NOT NULL,
   `materia` int(10) NOT NULL,
   `course` int(4) NOT NULL,
   `division` int(2) NOT NULL,
-  `schoolYear` int(4) NOT NULL,
+  `school_year` int(4) NOT NULL,
   `responsible` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
