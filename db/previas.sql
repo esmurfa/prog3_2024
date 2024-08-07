4c5536cf45be835da7e0e1e0fc8d72cd00a4f01d
Aron Mayer <121520247+AronMayer@users.noreply.github.com>
2024-08-07 20:35:15 -0300

    Update previas.sql
    Traducido al ingles

--- db/previas.sql
+++ db/previas.sql
@@ -1,18 +1,18 @@
-CREATE TABLE `notas_previas` (
+CREATE TABLE `exam_previous` (
   `id_student` int(8) NOT NULL,
   `id_subject` int(3) NOT NULL,
-  `nro_permiso` int(10) NOT NULL,
-  `fecha_examen` varchar(10) NOT NULL,
-  `nota` int(11) NOT NULL,
-  `presidente_mesa` varchar(100) NOT NULL,
-  `vocal1` varchar(100) NOT NULL,
-  `vocal2` varchar(100) NOT NULL
+  `permit_number` int(10) NOT NULL,
+  `date_exam` varchar(10) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `president_table` varchar(100) NOT NULL,
+  `vowel1` varchar(100) NOT NULL,
+  `vowel2` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `previas` (
-  `id_previa` int(10) NOT NULL,
-  `id_espacio` int(3) NOT NULL,
-  `ID_alumno` int(8) NOT NULL,
-  `anio` int(4) NOT NULL,
-  `estado` varchar(100) NOT NULL
+CREATE TABLE `exam` (
+  `id_exam_previous` int(10) NOT NULL,
+  `id_subjet` int(3) NOT NULL,
+  `id_student` int(8) NOT NULL,
+  `year` int(4) NOT NULL,
+  `state` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
