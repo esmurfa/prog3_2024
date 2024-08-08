0475b5e3b66892b605873ab18867f2d5cf61bcee
Aron Mayer <121520247+AronMayer@users.noreply.github.com>
2024-08-07 21:13:34 -0300

    Update assistance.sql
    Corregido el campo de materia a subject

--- db/assistance.sql
+++ db/assistance.sql
@@ -22,7 +22,7 @@ CREATE TABLE `counter_shift_assistance` (
   `turn` varchar(2) NOT NULL,
   `date` varchar(10) NOT NULL,
   `assistance` varchar(2) NOT NULL,
-  `materia` int(10) NOT NULL,
+  `subject` int(10) NOT NULL,
   `course` int(4) NOT NULL,
   `division` int(2) NOT NULL,
   `school_year` int(4) NOT NULL,
